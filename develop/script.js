ad3d42541954a7f2c134c8792b4e57f660f7c0b1
Luke Spang <lspang20@gmail.com>
2021-04-15 10:34:38 -0400

    thursday updates

--- develop/script.js
+++ develop/script.js
@@ -5,50 +5,57 @@ var newPass = ""
 
 function generatePassword (){
   var pwdLength = prompt("How long should your password be? Enter a length between 8 and 128.");
-}
 
-if (pwdLength < 8 || pwdLength > 128) {
-  alert("That is an invalid length. Try again")
-}
 
-if (pwdLength => 8 && pwdLength <= 128){
+  if (pwdLength < 8 || pwdLength > 128) {
+  alert("That is an invalid length. Try again");
+  return generatePassword();
+  }
+
+  if (pwdLength => 8 && pwdLength <= 128){
   low = confirm("Do you want to include lower case letters?")
   upp = confirm("Do you want to include upper case letters?")
   num = confirm("Do you want to include numbers?")
-  sym = confirm("Do you want to include special symbols?")
+  sym = confirm("Do you want to include special characters?")
+
+  if (low === false && upp === false && num === false && sym === false){
+    alert('You must select at least one criteria for your password. Try again!');
+    return generatePassword();
+  }
 
   if (low === true){
     lowerCase = "abcdefghijklmnopqrstuvwxyz"
   }
-    else {
+  else {
       lowerCase = ""
-    }
-    if (upp === true){
+  }
+  if (upp === true){
       upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    }
-      else {
+  }
+  else {
         lowerCase = ""
-      }
-      if (num === true){
+  }
+  if (num === true){
         numbers = "0123456789"
-      }
-        else {
-          lowerCase = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-        }
-        if (low === true){
-          symbols = ""
-        }
-          else {
-            symbols = ""
-          }
-          random = (lowerCase + upperCase + numbers + symbols)
+  }
+  else {
+          numbers = ""
+  }
+  if (sym === true){
+          symbols = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  }
+  else {
+        symbols = ""
+  }
+  random = (lowerCase + upperCase + numbers + symbols)
 
-for (let i = 0, n = random.length; i < pwdLength; ++i) {
-  newPass += random.charAt(Math.floor(Math.random() * n));
+for (let i = 0; i < pwdLength; ++i) {
+  newPass += random.charAt(Math.floor(Math.random() * random.length));
 }
 
 return newPass;
   
+}
 }
 
 
